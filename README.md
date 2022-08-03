f08bc2e17d9e6aa8a83473f1be452a3fdb5f7676
sugan0tech <sugankpms@gmail.com>
2022-08-03 20:18:31 +0530

    pushed to main py completely working with gTTS and s3

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # smart-polly
 Small python script for amazon polly
+
+features 
+by gTTS
+    - gender
+    - pitch
+    - speaking_rate
+    - audio profile (generates audio based of device needs like differend for ear buds and smartphone speakers)
+    - hertz rate
+    - volume db
+    - AudioEncoding
+
+others:
+    - audio play functionality while running scripts
+    - adding user profiles (includes default profile)
+    - local storage + aws s3
--- main.py
+++ main.py
@@ -1,73 +1,81 @@
-from cgitb import text
-from xmlrpc.client import Boolean
-from gtts import gTTS
-from dotenv import load_dotenv
 import os
-import ftplib
-import uuid
-import json
+import hashlib
+import boto3
+from google.cloud import texttospeech
 
-#storing text with corresponding audio
-def update_json(text, name) :
-    with open('data.json', 'r+') as f:
-        data = json.load(f)
-        data[text] = name 
-        f.seek(0)        
-        json.dump(data, f, indent=4)
-        f.truncate()     
-
-def check_json(text) -> bool:
-    with open('data.json', 'r') as f:
-        data = json.load(f)
-        if text in data:
-            return True
-        return False
-
-def play(name):
-    os.system(f"mpg123 {name}.mp3")
-
-def save_to_bunny(name, text):
-    if not check_json(text) :
-        session = ftplib.FTP(os.getenv("URL"),os.getenv("USERNAME"),os.getenv("PASSWORD"))
-        audio = open(f"{name}.mp3",'rb')                  
-        #sends the file
-        session.storbinary(f"STOR {name}.mp3", audio)     
-        audio.close()                                    
-        session.quit()
-        update_json(text, name)
-    else :
-        print("Already On BunnyCDN")
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "./key.json"
 
 
-load_dotenv()
+print("Welcome to the NeoTTS ;)")
+
+
+def upload_to_s3(name: str) -> None:
+
+    s3 = boto3.client("s3")
+
+    s3.upload_file(
+        Filename=f"./{name}.mp3",
+        Bucket="testvini",
+        Key=f"{name}.mp3",
+    )
+
+
+def get_name(text: str, pitch: int = 0, rate: int = 1, lang: str = 'en-US') -> str:
+    name = hashlib.md5(text.encode()).hexdigest()[:4] + \
+        f":p{pitch}:" + f"r{rate}:" + f"lang{lang}"
+    return name
+
+
+def run_speech(synthesis_input, pitch=0, speaking_rate=1, language_code='en-US', ssml_gender=texttospeech.SsmlVoiceGender.NEUTRAL):
+
+    voice = texttospeech.VoiceSelectionParams(
+        language_code='en-US',
+        ssml_gender=texttospeech.SsmlVoiceGender.NEUTRAL)
+    audio_config = texttospeech.AudioConfig(
+        pitch=pitch,
+        speaking_rate=speaking_rate,
+        audio_encoding=texttospeech.AudioEncoding.MP3
+    )
+    response = client.synthesize_speech(
+        input=synthesis_input,
+        voice=voice,
+        audio_config=audio_config)
+    return response
 
 
 if __name__ == "__main__":
-    print("Welcome to the NeoTTS ;)")
+    client = texttospeech.TextToSpeechClient()
+
     text_val = str(input("Enter the text :"))
-    print("Options:\n1.Play audio only\n2.Play audio and store\n3.Store Directly")
+    synthesis_input = texttospeech.SynthesisInput(text=text_val)
+    print("Options:\n",
+          "1.Make with default configuration\n",
+          "2.Or Custom configuration\n")
     option = int(input("Enter the option :"))
-    name = str(uuid.uuid4())
 
-    object = gTTS(text=text_val, slow=False)
-    object.save(f"{name}.mp3")
-
-
-    # Only plays mp3
     if(option == 1):
-        play(name)
+        response = run_speech(synthesis_input)
+        name = get_name(text_val)
 
-    #Plays and saving
     elif(option == 2):
-        play(name)
-        save_to_bunny(name, text_val)
+        language_code = str(input("Language :"))
+        pitch = int(input("Pitch range -20 to 20 :"))
+        speaking_rate = float(input("Speed range(.25 - 4.00) :"))
 
-    #Saving alone
-    elif(option == 3):
-        save_to_bunny(name, text_val)
+        response = run_speech(synthesis_input,
+                              language_code=language_code,
+                              pitch=pitch,
+                              speaking_rate=speaking_rate)
+        name = get_name(text_val,
+                        pitch=pitch,
+                        rate=speaking_rate,
+                        lang=language_code)
 
-    else:
-        print("Invalid Input")
+    with open(f'{name}.mp3', 'wb') as out:
+        # Write the response to the output file.
+        out.write(response.audio_content)
+        print(f'Audio content written to file "{name}.mp3"')
 
+    upload_to_s3(name)
 
     os.remove(f"{name}.mp3")
